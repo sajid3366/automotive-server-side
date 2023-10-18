107e8c3bd831cf999e6a84442aeba51c1df4e8c7
sajid360 <ahteshamsajid8@gmail.com>
2023-10-19 01:53:44 +0600

    search by brand name works

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -33,7 +33,55 @@ async function run() {
 
 
 
-       
+        app.get('/product', async (req, res) => {
+            const cursor = carCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        app.get('/product/:brand', async (req, res) => {
+            const brand = req.params.brand
+            const query = { brand: brand }
+            const result = await carCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: new ObjectId(id) }
+            const result = await carCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            console.log(newProduct);
+            const result = await carCollection.insertOne(newProduct);
+            res.send(result)
+        })
+
+        // app.put('/product/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const updatedCar = req.body;
+        //     const filter = { _id: new ObjectId(id) }
+        //     const options = { upsert: true };
+        //     const car = {
+        //         $set: {
+        //             name: updatedCar.name,
+        //             brand: updatedCar.brand,
+        //             type: updatedCar.type,
+        //             price: updatedCar.price,
+        //             description: updatedCar.description,
+        //             rating: updatedCar.rating,
+        //             photo: updatedCar.photo
+        //         }
+        //     }
+        //     const result = await carCollection.updateOne(filter, car, options)
+        //     res.send(result);
+
+        // })
+
+
 
         // for user collection
 
